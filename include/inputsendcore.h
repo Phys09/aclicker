d6f5a248fed94bd5c71da40ea57fa4beb39066c7
Michael <michaelkwan1238@gmail.com>
2026-08-29 16:28:12 -0400

    Update function signature of sending left clicks to windows

--- include/inputsendcore.h
+++ include/inputsendcore.h
@@ -5,7 +5,7 @@
 #include <windows.h>
 namespace aclicker {
 
-void SendLeftClicks(int n = 1, std::chrono::milliseconds input_delay =
-                                   std::chrono::milliseconds(25));
+void SendLeftClicks(int number_of_clicks = 1, std::chrono::milliseconds delay_between_clicks_ms = std::chrono::milliseconds(25),
+                    std::chrono::milliseconds start_delay = std::chrono::milliseconds(3000));
 int ParseAndSendInputToOS(int argc, std::span<std::string> argv);
 } // namespace aclicker
